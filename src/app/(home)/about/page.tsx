cce7631dcd11af7a87ec2fd44b78cce8d727ef19
MatheusKristman <kristman058@gmail.com>
2025-06-26 12:40:49 -0300

    03: home layout

---
+++ src/app/(home)/about/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <div>About page</div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/(home)/features/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <div>Features Page</div>
+    </div>
+  );
+};
+
+export default Page;
