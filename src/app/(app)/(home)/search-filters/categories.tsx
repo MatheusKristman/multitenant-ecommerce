e8efe9043ad8a16e35e7d729194386ad2fd33a9b
MatheusKristman <kristman058@gmail.com>
2025-06-27 11:36:38 -0300

    07: trpc-integration

--- src/app/(app)/(home)/search-filters/categories.tsx
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -4,13 +4,13 @@ import { ListFilterIcon } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 import { cn } from "@/lib/utils";
-import { CustomCategory } from "../types";
 import { Button } from "@/components/ui/button";
 import { CategoryDropdown } from "./category-dropdown";
 import { CategoriesSidebar } from "./categories-sidebar";
+import { CategoriesGetManyOutput } from "@/modules/categories/types";
 
 interface Props {
-  data: CustomCategory[];
+  data: CategoriesGetManyOutput;
 }
 
 export const Categories = ({ data }: Props) => {
@@ -60,7 +60,7 @@ export const Categories = ({ data }: Props) => {
   return (
     <div className="relative w-full">
       {/* Categories sidebar */}
-      <CategoriesSidebar data={data} open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
+      <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
 
       {/* Hidden div to measure all items */}
       <div
