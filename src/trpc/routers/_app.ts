fac09f823f1e211939dea3cb31b7b990365a6939
MatheusKristman <kristman058@gmail.com>
2025-06-30 21:42:10 -0300

    11: products

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,12 @@
 import { createTRPCRouter } from "../init";
+
 import { authRouter } from "@/modules/auth/server/procedures";
+import { productsRouter } from "@/modules/products/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
+  products: productsRouter,
   auth: authRouter,
 });
 
