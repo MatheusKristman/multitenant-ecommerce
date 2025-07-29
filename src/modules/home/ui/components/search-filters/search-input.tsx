ecbe9c9c3947f526d8133abface4aeab7bdea3e1
MatheusKristman <kristman058@gmail.com>
2025-07-29 19:27:22 -0300

    32-search-filters-fix

--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
 
@@ -13,13 +11,24 @@ import { useTRPC } from "@/trpc/client";
 
 interface Props {
     disabled?: boolean;
+    defaultValue?: string | undefined;
+    onChange?: (value: string) => void;
 }
 
-export const SearchInput = ({ disabled }: Props) => {
+export const SearchInput = ({ disabled, defaultValue, onChange }: Props) => {
+    const [searchValue, setSearchValue] = useState(defaultValue || "");
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const trpc = useTRPC();
     const session = useQuery(trpc.auth.session.queryOptions());
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            onChange?.(searchValue);
+        }, 500);
+
+        return () => clearTimeout(timeoutId);
+    }, [searchValue, onChange]);
 
     return (
         <div className="flex items-center gap-2 w-full">
@@ -35,6 +44,8 @@ export const SearchInput = ({ disabled }: Props) => {
                     className="pl-8"
                     placeholder="Search products"
                     disabled={disabled}
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
                 />
             </div>
 
